4c28e10ec3fd0d5f65181a790247ea0ee3f0c4ae
onelei <ahleiwolong@163.com>
2015-06-05 14:53:03 +0800

    Version1.0
    Save the file in the same dictionary, with the same file name. Such as
    *.xlsx to *.json

---
+++ ExcelToJsonAddIn/ExcelToJSonAddIn/ExcelToJSonAddIn/MyRibbon.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+using System.IO;
+namespace ExcelToJSonAddIn
+{
+    public partial class MyRibbon
+    {
+        private void MyRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnToJSon_Click(object sender, RibbonControlEventArgs e)
+        {
+            // When click the button, start switch;
+            ExcelToJSON();  
+        }
+
+
+        /**
+         * ExcelToJSON
+         * @function swich Excel to JSON; 
+         * return 
+         */
+        private string ExcelToJSON()
+        {
+            string text = "";
+            int totalRows = ThisAddIn.Instance.workBook.ActiveSheet.UsedRange.Rows.Count;
+            int totalColumns = ThisAddIn.Instance.workBook.ActiveSheet.UsedRange.Columns.Count;
+            text += "[{";
+            int startRow = 1;
+            int startColumn = 1;
+            for (int i = startRow+1; i != totalRows + 1; ++i)
+            {
+                for (int j = startColumn; j != totalColumns + 1; ++j)
+                {               
+                        text += "\"" + ThisAddIn.Instance.workBook.ActiveSheet.Cells(startRow, j).Value + "\":";
+                        // In the last row and last column, modify text style;
+                        if (i == totalRows && j == totalColumns)
+                        {
+                            text += "\"" + ThisAddIn.Instance.workBook.ActiveSheet.Cells(i, j).Value + "\"";
+                        }
+                        else
+                        {
+                            // Every Row Start, write "},";
+                            if (j == totalColumns)
+                            {
+                                text += "\"" + ThisAddIn.Instance.workBook.ActiveSheet.Cells(i, j).Value + "\"},{";
+                            }
+                            else
+                            {
+                                text += "\"" + ThisAddIn.Instance.workBook.ActiveSheet.Cells(i, j).Value + "\",";
+                            }
+                        }
+                    
+                }
+            }
+            text += "}]";
+
+            // Write to file;
+            string path = ThisAddIn.Instance.workBook.Path+"\\";
+
+            string fileName = ThisAddIn.Instance.workBook.Name;
+            fileName = fileName.Replace(".xlsx", "");
+            fileName += ".json";      
+            WriteToFile(path, fileName, text);
+            Message(totalRows, totalColumns);
+            return text;
+        }
+
+        /*
+         * Write to file
+         * @param path
+         * @param name
+         * @param text
+         * return
+         */
+        public void WriteToFile(string path, string name, string text)
+        {
+            lock (this)
+            {
+                // File stream information;
+                StreamWriter sw;
+                FileInfo t = new FileInfo(path+name);
+                // Create File Stream;
+                sw = t.CreateText();
+                // Write text by line style;
+                sw.WriteLine(text);
+                // Close stream;
+                sw.Close();
+                // Destrory stream;
+                sw.Dispose();
+            }
+
+        }
+
+        /*
+         * Show message.
+         */
+        private void Message(int totalRows, int totalColumns)
+        {
+            MessageBox.Show(
+                "\n"
+                +"  Excel to JSON\n"
+                +"  Created by OneLei.\n"
+                +"  Email: ahleiwolong@163.com \n"
+                +"  Copyright (c) 2014 Year. All rights reserved.\n\n"
+                +"  Save Successful!\n\n"
+                +"  Total: " + totalRows + " Rows"
+                +", " + totalColumns + " Columns");
+        }
+
+    }
+}
---
+++ ExcelToJsonAddIn/ExcelToJSonAddIn/ExcelToJSonAddIn/ThisAddIn.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+
+namespace ExcelToJSonAddIn
+{
+    public partial class ThisAddIn
+    {
+        public static ThisAddIn Instance;
+        public Excel.Workbook workBook;
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            Instance = this;
+            this.Application.WorkbookOpen += this.MyWorkBookOpen;
+        }
+
+        public void MyWorkBookOpen(Excel.Workbook Wb)
+        {
+            workBook = Wb;
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO 生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
